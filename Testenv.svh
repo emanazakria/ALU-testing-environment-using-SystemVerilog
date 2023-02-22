04f1e40ac1d1891094e7c1230cfa59850092bb96
eman zakria <82594179+emanazakria@users.noreply.github.com>
2023-02-22 22:59:10 +0200

    Update Testenv.svh

--- Testenv.svh
+++ Testenv.svh
@@ -1,5 +1,5 @@
 //includes all environment components:
-//Driver, Stimulus, Monitor, Scoreboard,?.
+//Driver, Stimulus, Monitor, Scoreboard,.
 //The objects creation and running for all components starts from here
 
 `include "alu_if.sv"
@@ -7,23 +7,23 @@ import env_pkg::*;
 
 class TestEnv;
 	// create handles
-	mailbox  s2d_mb				;
-	mailbox  log_mon, log_stim	;
+	mailbox  	s2d_mb				;
+	mailbox  	log_mon, log_stim		;
 
 	// dynamic array of handles
-	Driver alu_drv 				;
-	Monitor alu_mon 			;
+	Driver 		alu_drv 			;
+	Monitor 	alu_mon 			;
 
 	// Scoreboard & Stimulus Generator objects
-	Scoreboard sb 				;
-	Stimulus s 					;
+	Scoreboard 	sb 				;
+	Stimulus 	s 				;
 
 
 
 	// virtual interface 
-	virtual alu_if vif; 
+	virtual alu_if vif 				;	 
 
-	function new(virtual alu_if vif, int fd);
+	function new(virtual alu_if vif, int fd)	;
 
 		// create scoreboard & configure feeding mailbox handles
 		this.vif 	= vif 							;
@@ -33,10 +33,10 @@ class TestEnv;
 		log_stim 	= new()							;
 
 		// create the monitor, driver and stimulus objects
-		s 			= new (s2d_mb,log_stim)			;	 
-		alu_drv	 	= new (vif,s2d_mb)				;
-		alu_mon 	= new (vif,log_mon)				;
-		sb 			= new (log_mon,log_stim,fd)		;
+		s 		= new (s2d_mb,log_stim)					;	 
+		alu_drv	 	= new (vif,s2d_mb)					;
+		alu_mon 	= new (vif,log_mon)					;
+		sb 		= new (log_mon,log_stim,fd)				;
 		
 	endfunction
 
@@ -44,11 +44,11 @@ class TestEnv;
 	task run();
 		// start everything
 		fork
-			s.stim_task(100) 				; // number of trans
-			alu_drv.driver_task()			;
-			alu_mon.input_monitor_task()	;
-			alu_mon.output_monitor_task()	;
-			sb.sb_task()					;
+			s.stim_task(100) 						; // number of trans
+			alu_drv.driver_task()						;
+			alu_mon.input_monitor_task()					;
+			alu_mon.output_monitor_task()					;
+			sb.sb_task()							;
 		join 
 			$finish;
 	endtask
