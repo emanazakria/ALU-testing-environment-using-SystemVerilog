5d3326e05fe0a59c6719572d9f9005f2d9717348
eman zakria <82594179+emanazakria@users.noreply.github.com>
2023-02-22 22:57:38 +0200

    Update Stimulus_gen.svh

--- Stimulus_gen.svh
+++ Stimulus_gen.svh
@@ -5,28 +5,28 @@
 class Stimulus;
 
 	Packet 	pkt 			;
-	mailbox s2d_mb,log_stim	;
-    int 	i = 0			;
+	mailbox s2d_mb,log_stim		;
+    	int 	i = 0			;
 
 
-	function new(mailbox s2d_mb, mailbox log_stim);
-		this.s2d_mb 	= s2d_mb 	;
-		this.log_stim 	= log_stim 	;
+	function new(mailbox s2d_mb, mailbox log_stim)	;
+		this.s2d_mb 	= s2d_mb 		;
+		this.log_stim 	= log_stim 		;
 	endfunction
 
 	task stim_task(int no_of_trans);
 		repeat(no_of_trans)  begin
 		
 				pkt = new() 				;
-				i ++						;
+				i ++					;
 				pkt.pkt_num = i				;
 				
 				//wait for driver to put the data on the interface
-				wait (s2d_mb.num () == 0) 	; 
-				pkt.randomize()				; 
+				wait (s2d_mb.num () == 0) 		; 
+				pkt.randomize()				;	 
 				
 				//for debugging
-				$display("packet = %p",pkt)	;
+				$display("packet = %p",pkt)		;
 				
 				//sending the packet to driver and scoreboard
 				s2d_mb.put(pkt)				;
