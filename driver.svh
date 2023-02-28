9219dcc40577eab8d1d430598e9833bc1ec0265f
eman zakria <82594179+emanazakria@users.noreply.github.com>
2023-02-28 13:25:50 +0200

    Update driver.svh

--- driver.svh
+++ driver.svh
@@ -7,12 +7,12 @@ class Driver;
 	
 	virtual alu_if vif	;
 	mailbox s2d_mb		;
-	Packet 	pkt			;
+	Packet 	pkt		;
 	 
 
 	function new(virtual alu_if vif , mailbox s2d_mb );
 		this.vif 	= vif		;
-		this.s2d_mb = s2d_mb	;
+		this.s2d_mb 	= s2d_mb	;
 	endfunction
 	
 
@@ -31,24 +31,24 @@ class Driver;
 		begin
 			vif.a 				= pkt.a 			;
 			vif.b 				= pkt.b 			;
-			vif.valid_in 		= pkt.valid_in 		;
+			vif.valid_in 			= pkt.valid_in 			;
 			vif.cin 			= pkt.cin 			;
 			vif.ctl 			= pkt.ctl			;
-			vif.pkt_num     	= pkt.pkt_num  		;
+			vif.pkt_num     		= pkt.pkt_num  			;
  		end
 
 		//output (just to print the packet - not needed)
 		@(posedge vif.clk)
 		begin
 			pkt.alu 			= vif.alu 			;
-			pkt.valid_out 		= vif.valid_out 	;
-			pkt.carry 			= vif.carry 		;
+			pkt.valid_out 			= vif.valid_out 		;
+			pkt.carry 			= vif.carry 			;
 			pkt.zero 			= vif.zero 			;
 		end
 
 
 		s2d_mb.get(pkt)								;
-		$display("packet_driver = %p",pkt)			;	
+		$display("packet_driver = %p",pkt)					;	
  
 		end 
 	endtask 
